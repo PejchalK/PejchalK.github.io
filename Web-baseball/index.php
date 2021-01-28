43d54f47558b755f0d0840c9404aef12cba5b40a
PejchalK <pejchyc@seznam.cz>
2021-01-28 10:10:24 +0100

    1st commit

---
+++ Web-baseball/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <header>
+        <a href="https://www.facebook.com/kamil.pejchal" class="fa fa-facebook"></a>
+        <div class="frame">
+            <div class="text">
+                <h1> About me... </h1>
+                    Etiam quis nisl odio. Nullam consequat mollis ullamcorper. Mauris rutrum sapien eu semper tempus. Pellentesque mi nisl, sollicitudin at neque non, vestibulum laoreet lorem. Ut at fringilla purus. Fusce in nunc eget sapien feugiat porttitor. Proin eleifend ipsum turpis, eget consequat dui mollis eu.
+            </div>
+        </div>
+
+
+<a href="https://www.facebook.com/kamil.pejchal" class="facebook">
+    <h2> Contact me...</h2>
+    <svg width="32" height="32" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg" fill="#f3f4f7">
+        <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z"/>
+    </svg>
+</a>
+
+  
+    </header>
+<div class="menu-under">
+    <div class="block">
+        Catch&Throw
+    </div>
+    <div class="block">
+        Hitting
+    </div>
+    <div class="block">
+        Pitching&Velo
+    </div>
+    <div class="block">
+        Fielding
+    </div>
+</div>
+<div class="aboutme">
+        <table>
+  <tr>
+    <th>Year</th>
+    <th>G</th>
+    <th>AVG</th>
+    <th>SLG</th>
+    <th>OBP</th>
+  </tr>
+  <tr>
+    <td>2019</td>
+    <td>23</td>
+    <td>.278</td>
+    <td>.329</td>
+    <td>.367</td>
+  </tr>
+  <tr>
+    <td>2018</td>
+    <td>21</td>
+    <td>.208</td>
+    <td>.208</td>
+    <td>.310</td>
+  </tr>
+  <tr>
+    <td>2017</td>
+    <td>22</td>
+    <td>.114</td>
+    <td>.129</td>
+    <td>.182</td>
+  </tr>
+</table>
+    </div>
+
+
+<article>
+<div class="frame">
+    <iframe src="https://www.youtube.com/embed/cmc8q2dcIMs&loop=1">
+    </iframe>
+        article blabla
+            <div class="spot">
+
+            </div>
+</div>
+</article>
+</body>
+
+</html>
